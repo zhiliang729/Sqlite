b21d0b56626daaff51cfc9740017ef77d99d40dc
zhiliang <zhiliang729@163.com>
2013-10-10 09:45:24 +0800

    Initial Commit

---
+++ Sqlite/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Sqlite
+//
+//  Created by zhangliang on 13-10-10.
+//  Copyright (c) 2013年 com.gooagoo.Gooagoo. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
